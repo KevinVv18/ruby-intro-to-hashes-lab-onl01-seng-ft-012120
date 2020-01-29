2f0aa55dc65cd34f9fe49f28305ec57bcd17d5bd
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-29 19:07:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,15 +23,11 @@ monopoly = {:railroads=> {:pieces =>4, :names => {:reading_railroad=> {}, :penns
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200}}}
-
-
-
-
-
-
 end
 
 def monopoly_with_fourth_tie
+  
+  
   
 
 
